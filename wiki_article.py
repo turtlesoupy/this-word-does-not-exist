7e7b6b8f72dd3bc83e512f20c7950db88e7b0dc2
Thomas Dimson <tdimson@gmail.com>
2020-03-08 22:09:04 -0700

    Language modeling evaluation confirmed

--- wiki_article.py
+++ wiki_article.py
@@ -5,6 +5,9 @@ from dataclasses import dataclass
 from io import StringIO
 from transformers import AutoModelWithLMHead, AutoTokenizer
 from scipy import stats
+from torch.nn.utils.rnn import pad_sequence
+from typing import List
+from torch.utils.data import SequentialSampler, DataLoader
 
 @dataclass
 class Article:
@@ -59,7 +62,7 @@ def generate_text_dataset(istream, ostream, offset=0, stride=1024, limit=None):
         else:
             ostream.write(text)
             ostream.write(title_tokenization(article.title))       
-    
+    extract_dictionary
     for article in refine_wikitext(istream, limit=limit):
         if offset > 0:
             _output_range(article, 0, offset)
@@ -67,7 +70,6 @@ def generate_text_dataset(istream, ostream, offset=0, stride=1024, limit=None):
         for i in range(offset, len(article.text), stride):
             _output_range(article, i, i + stride)       
         
-
 def title_perplexity(model, tokenizer, article, device='cuda'):
     max_length = model.config.n_positions
     article_tokens = tokenizer.tokenize(article.text)
@@ -94,6 +96,47 @@ def title_perplexity(model, tokenizer, article, device='cuda'):
         title_pp = -lp /  n
     
     return title_pp.item()
+
+
+def lm_eval(model, tokenizer, file_path, device='cuda', block_size=512, batch_size=1):
+    def collate(examples: List[torch.Tensor]):
+        if tokenizer._pad_token is None:
+            return pad_sequence(examples, batch_first=True)
+        return pad_sequence(examples, batch_first=True, padding_value=tokenizer.pad_token_id)
+    
+    block_size = block_size - (tokenizer.max_len - tokenizer.max_len_single_sentence)
+    eval_dataset = []
+    with open(file_path, encoding="utf-8") as f:
+        text = f.read()
+        tokenized_text = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
+        for i in range(0, len(tokenized_text) - block_size + 1, block_size):  # Truncate in block of block_size
+            tokenized =  tokenizer.build_inputs_with_special_tokens(tokenized_text[i : i + block_size])
+            tensorized = torch.tensor(tokenized, dtype=torch.long)
+            eval_dataset.append(tensorized)
+        
+    eval_sampler = SequentialSampler(eval_dataset)
+    eval_dataloader = DataLoader(
+        eval_dataset, sampler=eval_sampler, batch_size=batch_size, collate_fn=collate
+    )
+    
+    for batch in eval_dataloader:
+        inputs, labels = (batch, batch)
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        
+    eval_loss = 0.0
+    with torch.no_grad():
+        outputs = model(inputs, labels=labels)
+        lm_loss = outputs[0]
+        eval_loss += lm_loss.mean().item()
+        
+    perplexity = torch.exp(torch.tensor(eval_loss))
+    
+    return perplexity
+
+        
+        
+        
 
 
 def perplexity(sentence):
