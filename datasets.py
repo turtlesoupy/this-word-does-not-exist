0a4a31923d06fd222f5972441964714a75153d27
Thomas Dimson <tdimson@gmail.com>
2020-03-23 21:49:23 -0700

    Add datasets

---
+++ datasets.py
@@ -0,0 +1,366 @@
+import types
+import os
+import logging
+import random
+import pickle
+import hashlib
+import string
+import itertools
+import dictionary_definition
+from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer
+from typing import NamedTuple, List, Optional
+
+logger = logging.getLogger(__name__)
+
+
+def _split_range(splits, split_idx):
+    splits_tensor = torch.tensor(splits)
+    sum_splits = torch.cumsum(splits_tensor, 0)
+
+    if sum_splits[-1] != 1.0:
+        raise RuntimeError(f"Splits must sum to 1 (actual: {sum_splits[-1]})")
+    elif split_idx >= len(sum_splits):
+        raise RuntimeError(f"Invalid split index {split_idx} (must be less than {len(sum_splits)})")
+
+    if split_idx == 0:
+        start_range = 0.0
+    else:
+        start_range = sum_splits[split_idx - 1]
+
+    end_range = sum_splits[split_idx]
+
+    return (start_range, end_range)
+
+
+def _in_split_range(split_range, randomizer_str):
+    def in_split(dictionary_definiton):
+        val = int(hashlib.md5(randomizer_str.encode("utf-8")).hexdigest(), 16,) % 100000 / 100000
+        return (val >= start_range and val < end_range).item()
+
+
+def _cache_path(base_directory, filename, **keys):
+    path = [base_directory]
+    for k, v in keys:
+        if isinstance(v, types.StringTypes):
+            path.append(f"_{k}-{v}_")
+            continue
+
+        try:
+            path.append(f"_{k}-{'-'.join(str(e) for e in iter(v))})_")
+        except TypeError:
+            pass
+
+        path.append(f"_{k}-{str(v)}_")
+
+    path.append(filename)
+    return os.path.join(path)
+
+
+class TokenGroup(NamedTuple):
+    separator: List[int] = []
+    payload: List[int] = []
+    remove_if_truncated: bool = False
+
+
+def _join_and_truncate(
+    max_len: int, begin_tokens: List[int], token_groups: List[TokenGroup], end_tokens: List[int], min_append_size=5
+):
+    if len(begin_tokens) + len(end_tokens) > max_len:
+        raise RuntimeError("Length is too small for required tokens")
+
+    running_max_len = max_len - len(begin_tokens) - len(end_tokens)
+
+    ret = [begin_tokens]
+
+    for token_group in token_groups:
+        if len(token_group.separator) + len(token_group.payload) > running_max_len:
+            if token_group.remove_if_truncated:
+                break
+
+            if running_max_len - len(token_group.separator) - len(token_group.payload) < min_append_size:
+                break
+
+            ret.append(token_group.separator)
+            running_max_len -= len(token_group.separator)
+            ret.append(token_group.payload[:running_max_len])
+            running_max_len = 0
+            break
+        else:
+            ret.append(token_group.separator)
+            ret.append(token_group.payload)
+            running_max_len -= len(token_group.separator) + len(token_group.payload)
+
+    ret.append(end_tokens)
+    return list(itertools.chain.from_iterable(ret))
+
+
+class SpecialTokens:
+    BOS_TOKEN = "<|bod|>"
+    EOS_TOKEN = "<|eod|>"
+    PAD = "<|pad|>"
+
+    DEFINITION_SEP = "<|bd|>"
+    EXAMPLE_SEP = "<|be|>"
+    POS_SEP = "<|pos|>"
+    TOPIC_SEP = "<|bto|>"
+
+    @classmethod
+    def special_tokens_dict(cls):
+        return {
+            "bos_token": cls.BOS_TOKEN,
+            "eos_token": cls.EOS_TOKEN,
+            "pad_token": cls.PAD,
+            "additional_special_tokens": [cls.DEFINITION_SEP, cls.EXAMPLE_SEP, cls.POS_SEP, cls.TOPIC_SEP],
+        }
+
+
+class ParsedDictionaryDefinitionDataset(Dataset):
+    def _make_examples(self, tokenizer, entry: dictionary_definition.Entry):
+        examples = []
+        for sense in entry.senses:
+            for definition in sense.definitions:
+                if isinstance(definition, dictionary_definition.ReferenceDefinition):
+                    continue
+
+                token_groups = []
+                token_groups.append(TokenGroup(separator=[], payload=tokenizer.encode(entry.word)))
+
+                if sense.pos:
+                    if definition.pos_modifier:
+                        payload = tokenizer.encode(f"{sense.pos} {definition.pos_modifier}")
+                    else:
+                        payload = tokenizer.encode(sense.pos)
+
+                    token_groups.append(TokenGroup(separator=self.pos_sep_ids, payload=payload))
+
+                if definition.topic:
+                    token_groups.append(
+                        TokenGroup(separator=self.topic_sep_ids, payload=tokenizer.encode(definition.topic))
+                    )
+
+                token_groups.append(
+                    TokenGroup(separator=self.definition_sep_ids, payload=tokenizer.encode(definition.definition))
+                )
+
+                for example in definition.examples:
+                    token_groups.append(
+                        TokenGroup(
+                            separator=self.example_sep_ids, payload=tokenizer.encode(example), remove_if_truncated=True,
+                        )
+                    )
+
+                example = _join_and_truncate(
+                    max_len=self.max_len,
+                    begin_tokens=self.bos_token_ids,
+                    end_tokens=self.eos_token_ids,
+                    token_groups=token_groups,
+                )
+
+                assert (
+                    len(example) <= self.max_len
+                ), f"Example should be less than max length: {len(example)} Vs. {self.max_len}"
+
+                examples.append(example)
+
+        return examples
+
+    def __init__(
+        self, tokenizer: PreTrainedTokenizer, args, file_path: str, splits=(1.0), split_idx=0,
+    ):
+        # self.max_len = min(tokenizer.max_len_single_sentence, args.block_size)
+        self.max_len = 768
+        self.bos_token_ids = tokenizer.encode(SpecialTokens.BOS_TOKEN)
+        self.eos_token_ids = tokenizer.encode(SpecialTokens.EOS_TOKEN)
+        self.pos_sep_ids = tokenizer.encode(SpecialTokens.POS_SEP)
+        self.definition_sep_ids = tokenizer.encode(SpecialTokens.DEFINITION_SEP)
+        self.example_sep_ids = tokenizer.encode(SpecialTokens.EXAMPLE_SEP)
+        self.topic_sep_ids = tokenizer.encode(SpecialTokens.TOPIC_SEP)
+        return
+
+        assert os.path.isfile(file_path) or os.path.islink(file_path)
+        directory, filename = os.path.split(file_path)
+
+        cached_features_file = _cache_path(
+            directory, filename, splits=splits, split_idx=split_idx, max_len=self.max_len,
+        )
+
+        if os.path.exists(cached_features_file) and not args.overwrite_cache:
+            logger.info("Loading features from cached file %s", cached_features_file)
+            with open(cached_features_file, "rb") as handle:
+                self.examples = pickle.load(handle)
+            logger.info("Loaded {len(self.examples)} features")
+        else:
+            logger.info(
+                f"Cache at {cached_features_file} not found... creating features from dataset file at %s", directory
+            )
+
+            self.examples = []
+            split_range = _split_range(splits, split_idx)
+
+            with open(file_path, "rb") as f:
+                words = list(pickle.load(f).values())
+
+            for word in words:
+                if _in_split(word.title):
+                    self.examples.extend(self._make_examples(tokenizer, word))
+
+            logger.info(f"Saving {len(self.examples)} features into cached file {cached_features_file}")
+            with open(cached_features_file, "wb") as handle:
+                pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def __len__(self):
+        return len(self.examples)
+
+
+class BinaryDictionaryDefinitionDataset(Dataset):
+    @classmethod
+    def title_tokenization(cls, title):
+        return f"<title>{title}</title>"
+
+    @classmethod
+    def _make_example(cls, tokenizer, definition):
+        max_len = self.max_len
+
+        m = re.match(r"\s*" + re.escape(definition.title) + r"\d*\s*(\|[^|]*\|)?\s*", definition.entry_str,)
+        if m:
+            trainable_entry = definition.entry_str[m.span()[1] :].strip()
+            if not trainable_entry:
+                raise RuntimeError(f"Bad entry for {definition.title}: '{definition.entry_str}'")
+        else:
+            raise RuntimeError(f"Couldn't match {definition.title} on '{definition.entry_str}'")
+
+        tokenized_title = [tokenizer.bos_token_id] + tokenizer.convert_tokens_to_ids(
+            tokenizer.tokenize(cls.title_tokenization(definition.title))
+        )
+        tokenized_entry = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(trainable_entry))
+
+        if len(tokenized_title) + len(tokenized_entry) > max_len:
+            logger.warn(f"Truncating long entry for '{definition.title}' (entry is {len(tokenized_entry)})")
+
+        all_tokenized = (tokenized_title + tokenized_entry)[:max_len]
+        example = tokenizer.build_inputs_with_special_tokens(all_tokenized)
+        assert len(example) == len(all_tokenized), "If this fails our tokenizer is weird"
+        bool_mask = [bool(i > 1 and i <= len(tokenized_title)) for i in range(len(example))]
+
+        return (example, bool_mask)
+
+    def __init__(
+        self, tokenizer: PreTrainedTokenizer, args, file_path: str, splits=(1.0), split_idx=0,
+    ):
+        assert os.path.isfile(file_path) or os.path.islink(file_path)
+
+        self.max_len = min(tokenizer.max_len_single_sentence, args.block_size)
+
+        directory, filename = os.path.split(file_path)
+        cached_features_file = _cache_path(
+            directory, filename, splits=splits, split_idx=split_idx, max_len=self.max_len,
+        )
+
+        if os.path.exists(cached_features_file) and not args.overwrite_cache:
+            logger.info("Loading features from cached file %s", cached_features_file)
+            with open(cached_features_file, "rb") as handle:
+                self.examples = pickle.load(handle)
+            logger.info("Loaded {len(self.examples)} features")
+        else:
+            logger.info("Creating features from dataset file at %s", directory)
+
+            split_range = _split_range(splits, split_idx)
+            self.examples = []
+
+            with open(file_path, "rb") as f:
+                for dictionary_definition in DictionaryDefinition.gen_from_apple_dictionary(f):
+                    if _in_split(split_range, dictionary_definition.title):
+                        self.examples.append(self._make_example(tokenizer, dictionary_definition))
+
+            logger.info(f"Saving {len(self.examples)} features into cached file {cached_features_file}")
+            with open(cached_features_file, "wb") as handle:
+                pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, item):
+        return (
+            torch.tensor(self.examples[item][0], dtype=torch.long),
+            torch.tensor(self.examples[item][1], dtype=torch.bool),
+        )
+
+
+class UrbanDictionaryDataset(Dataset):
+    def _make_examples(self, tokenizer, word):
+        examples = []
+
+        for definition in word.definitions:
+            example = _join_and_truncate(
+                max_len=self.max_len,
+                begin_tokens=self.bos_token_ids,
+                end_tokens=self.eos_token_ids,
+                token_groups=[
+                    TokenGroup(separator=[], payload=tokenizer.encode(definition.word)),
+                    TokenGroup(separator=self.definition_sep_ids, payload=tokenizer.encode(definition.meaning)),
+                    TokenGroup(separator=self.example_sep_ids, payload=tokenizer.encode(definition.examples[0])),
+                ],
+            )
+
+            bool_mask = [
+                bool(i >= len(self.bos_token_ids) and i < (len(self.bos_token_ids) + len(title_tokenization)))
+                for i in range(len(example))
+            ]
+
+            assert (
+                len(example) <= self.max_len
+            ), f"Example should be less than max length: {len(example)} Vs. {self.max_len}"
+
+            examples.append((example, bool_mask))
+
+        return examples
+
+    def __init__(
+        self, tokenizer: PreTrainedTokenizer, args, file_path: str, splits=(1.0), split_idx=0,
+    ):
+        self.max_len = min(tokenizer.max_len_single_sentence, args.block_size)
+        self.bos_token_ids = tokenizer.encode(SpecialTokens.BOS_TOKEN)
+        self.eos_token_ids = tokenizer.encode(SpecialTokens.EOS_TOKEN)
+        self.definition_sep_ids = tokenizer.encode(SpecialTokens.DEFINITION_SEP)
+        self.example_sep_ids = tokenizer.encode(SpecialTokens.EXAMPLE_SEP)
+
+        assert os.path.isfile(file_path) or os.path.islink(file_path)
+        directory, filename = os.path.split(file_path)
+
+        cached_features_file = _cache_path(
+            directory, filename, splits=splits, split_idx=split_idx, max_len=self.max_len,
+        )
+
+        if os.path.exists(cached_features_file) and not args.overwrite_cache:
+            logger.info("Loading features from cached file %s", cached_features_file)
+            with open(cached_features_file, "rb") as handle:
+                self.examples = pickle.load(handle)
+            logger.info("Loaded {len(self.examples)} features")
+        else:
+            logger.info(
+                f"Cache at {cached_features_file} not found... creating features from dataset file at %s", directory
+            )
+
+            self.examples = []
+            split_range = _split_range(splits, split_idx)
+
+            with open(file_path, "rb") as f:
+                words = list(pickle.load(f).values())
+
+            for word in words:
+                if _in_split(word.title):
+                    self.examples.extend(self._make_examples(tokenizer, word))
+
+            logger.info(f"Saving {len(self.examples)} features into cached file {cached_features_file}")
+            with open(cached_features_file, "wb") as handle:
+                pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, item):
+        return (
+            torch.tensor(self.examples[item][0], dtype=torch.long),
+            torch.tensor(self.examples[item][1], dtype=torch.bool),
+        )
--- dictionary_definition.py
+++ dictionary_definition.py
@@ -215,7 +215,7 @@ class AppleDictParser:
                 elif not c.get_text().strip():
                     continue
                 elif "se2" in c["class"]:
-                    sense_definitions.append(cls.parse_sense_definitions(c))
+                    sense_definitions.extend(cls.parse_sense_definitions(c))
                 elif "note" in c["class"]:
                     logger.warning(f"Dropping note in word sense {c}")
                 elif "msDict" in c["class"]:
@@ -316,112 +316,6 @@ class AppleDictParser:
             origin=origin,
             derivatives=derivatives,
             notes=notes,
-        )
-
-
-class DictionaryDefinitionDataset(Dataset):
-    @classmethod
-    def title_tokenization(cls, title):
-        return f"<title>{title}</title>"
-
-    @classmethod
-    def _make_example(cls, tokenizer, definition):
-        max_len = self.max_len
-
-        m = re.match(r"\s*" + re.escape(definition.title) + r"\d*\s*(\|[^|]*\|)?\s*", definition.entry_str,)
-        if m:
-            trainable_entry = definition.entry_str[m.span()[1] :].strip()
-            if not trainable_entry:
-                raise RuntimeError(f"Bad entry for {definition.title}: '{definition.entry_str}'")
-        else:
-            raise RuntimeError(f"Couldn't match {definition.title} on '{definition.entry_str}'")
-
-        tokenized_title = [tokenizer.bos_token_id] + tokenizer.convert_tokens_to_ids(
-            tokenizer.tokenize(cls.title_tokenization(definition.title))
-        )
-        tokenized_entry = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(trainable_entry))
-
-        if len(tokenized_title) + len(tokenized_entry) > max_len:
-            logger.warn(f"Truncating long entry for '{definition.title}' (entry is {len(tokenized_entry)})")
-
-        all_tokenized = (tokenized_title + tokenized_entry)[:max_len]
-        example = tokenizer.build_inputs_with_special_tokens(all_tokenized)
-        assert len(example) == len(all_tokenized), "If this fails our tokenizer is weird"
-        bool_mask = [bool(i > 1 and i <= len(tokenized_title)) for i in range(len(example))]
-
-        return (example, bool_mask)
-
-    def __init__(
-        self, tokenizer: PreTrainedTokenizer, args, file_path: str, splits=(1.0), split_idx=0,
-    ):
-        assert os.path.isfile(file_path) or os.path.islink(file_path)
-
-        self.max_len = min(tokenizer.max_len_single_sentence, args.block_size)
-
-        directory, filename = os.path.split(file_path)
-
-        cached_features_file = os.path.join(
-            directory,
-            args.model_type
-            + "_cached_lm_splits_"
-            + "_".join(str(e) for e in splits)
-            + "_split_idx_"
-            + str(split_idx)
-            + "_max_len_"
-            + str(self.max_len)
-            + "_"
-            + filename,
-        )
-
-        splits_tensor = torch.tensor(splits)
-        sum_splits = torch.cumsum(splits_tensor, 0)
-
-        if sum_splits[-1] != 1.0:
-            raise RuntimeError(f"Splits must sum to 1 (actual: {sum_splits[-1]})")
-        elif split_idx >= len(sum_splits):
-            raise RuntimeError(f"Invalid split index {split_idx} (must be less than {len(sum_splits)})")
-
-        if split_idx == 0:
-            start_range = 0.0
-        else:
-            start_range = sum_splits[split_idx - 1]
-
-        end_range = sum_splits[split_idx]
-
-        def in_split(dictionary_definiton):
-            val = int(hashlib.md5(dictionary_definition.title.encode("utf-8")).hexdigest(), 16,) % 10000 / 10000
-            return (val >= start_range and val < end_range).item()
-
-        if os.path.exists(cached_features_file) and not args.overwrite_cache:
-            logger.info("Loading features from cached file %s", cached_features_file)
-            with open(cached_features_file, "rb") as handle:
-                self.examples = pickle.load(handle)
-            logger.info("Loaded {len(self.examples)} features")
-        else:
-            logger.info("Creating features from dataset file at %s", directory)
-
-            self.examples = []
-
-            with open(file_path, "rb") as f:
-                for dictionary_definition in DictionaryDefinition.gen_from_apple_dictionary(f):
-                    if in_split(dictionary_definition):
-                        self.examples.append(self._make_example(tokenizer, dictionary_definition))
-
-            # Note that we are loosing the last truncated example here for the sake of simplicity (no padding)
-            # If your dataset is small, first you should loook for a bigger one :-) and second you
-            # can change this behavior by adding (model specific) padding.
-
-            logger.info(f"Saving {len(self.examples)} features into cached file {cached_features_file}")
-            with open(cached_features_file, "wb") as handle:
-                pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    def __len__(self):
-        return len(self.examples)
-
-    def __getitem__(self, item):
-        return (
-            torch.tensor(self.examples[item][0], dtype=torch.long),
-            torch.tensor(self.examples[item][1], dtype=torch.bool),
         )
 
 
