1095964387cdb0c3d2400bc18c957ade0f7c29b4
Thomas Dimson <tdimson@gmail.com>
2020-05-07 00:32:12 -0700

    Word service

---
+++ word_service/gen_stubs.sh
@@ -0,0 +1,8 @@
+python -m grpc_tools.protoc \
+    --include_imports \
+    --include_source_info \
+    --proto_path=. \
+    --python_out=. \
+    --grpc_python_out=. \
+    --descriptor_set_out=api_descriptor.pb \
+    bookstore.proto
